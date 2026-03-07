4f6af8999f92dab18cda91860b11f05e928d2ffd
chinmayee123456 <danyashrichinu@gmail.com>
2026-03-07 12:54:39 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+// Import Firebase SDKs (via CDN)
+const firebaseConfig = {
+  apiKey: "AIzaSyAajQDEx-_EIWI0Ur1QiHWuc1v2Gq_qsRc",
+  authDomain: "pupa-pet.firebaseapp.com",
+  projectId: "pupa-pet",
+  storageBucket: "pupa-pet.firebasestorage.app",
+  messagingSenderId: "782314476536",
+  appId: "1:782314476536:web:92f6d14f6c670f655abc0d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
