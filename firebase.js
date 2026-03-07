79b6f25e9a7b3c57f0d1bf69b93f76896ff323f8
chinmayee123456 <danyashrichinu@gmail.com>
2026-03-07 16:02:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,14 @@
 // firebase.js
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  
+  apiKey: "AIzaSyAajQDEx-_EIWI0Ur1QiHWuc1v2Gq_qsRc",
+  authDomain: "pupa-pet.firebaseapp.com",
+  projectId: "pupa-pet",
+  storageBucket: "pupa-pet.firebasestorage.app",
+  messagingSenderId: "782314476536",
+  appId: "1:782314476536:web:92f6d14f6c670f655abc0d"
+};
+
 };
 
 // Initialize Firebase
